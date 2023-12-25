78b50996a6528b4efa63db0932466e88e49c5603
Abdullahi Abdi <abdullahiabdi1233@gmail.com>
2023-12-25 23:57:20 +0300

    Changed variable types, added array operations, and modified variable scope. (generated by blackbox.ai )

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
+
 fn main() {
 
     let i: &str = "t";
     let u: String = String::from("Start");
+    let array = [10,10,10];
+    println!("{:?}", array);
+    let array : [i64; 2]= [1, 10];
+    println!("{:?}", array);
+    let slice = &array[0..1];
+    println!("{:?}", slice);
+    let number = array.len();
+    println!("{}", number);
 
+    {
+
+    let mut u = u.clone();
+
+    u = String::from("Start Now");
+
+    println!("{}", u);
+    }
+    {
+
+    let mut i = i;
+
+    i = "S";
+    }
+
+    println!("{}", i);
+    
 
     if u == i  {
         println!("X is a String. i = {}. u = {} ", i, u);
@@ -10,6 +36,8 @@ fn main() {
     else {
         println!("X is not a String i = {}. u = {}", i, u);
     }
+
+    println!("Start");
 
     println!("Hello, world!");
 }
