82fc2c55a39da8a9f969ec85ff51548034a7d573
Abdullahi Abdi <abdullahiabdi1233@gmail.com>
2023-12-25 23:25:37 +0300

    learning

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    let i: &str = "t";
+    let u: String = String::from("Start");
+
+
+    if u == i  {
+        println!("X is a String. i = {}. u = {} ", i, u);
+    }
+    else {
+        println!("X is not a String i = {}. u = {}", i, u);
+    }
+
+    println!("Hello, world!");
+}
