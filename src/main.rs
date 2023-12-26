c233ab858b70a19aa618ba3e22360ab1d4ed472d
Abdullahi Abdi <abdullahiabdi1233@gmail.com>
2023-12-26 15:10:01 +0300

    Concatenate two strings

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,14 @@
+fn concatenate_strings (a: &str, b:&str) -> String {
+    let mut result = String::from(a);
+    result.push_str(b);
+    result
+}
 
 fn main() {
+    let string1: String = String::from("Hello");
+    let string2: String = String::from("World");
 
-    let i: &str = "t";
-    let u: String = String::from("Start");
-    let array = [10,10,10];
-    println!("{:?}", array);
-    let array : [i64; 2]= [1, 10];
-    println!("{:?}", array);
-    let slice = &array[0..1];
-    println!("{:?}", slice);
-    let number = array.len();
-    println!("{}", number);
-	let _a = 10987;
-
-    {
-
-    let mut u = u.clone();
-
-    u = String::from("Start Now");
-
-    println!("{}", u);
-    }
-    {
-
-    let mut i = i;
-
-    i = "S";
-
-    println!("{}", i);
-    }
-
-    if u == i  {
-        println!("X is a String. i = {}. u = {} ", i, u);
-    }
-    else {
-        println!("X is not a String i = {}. u = {}", i, u);
-    }
-
-    println!("Start");
-
-    println!("Hello, world!");
+    let result = concatenate_strings(string1.as_str(), string2.as_str());
+    println!("{}", result);
+     
 }
